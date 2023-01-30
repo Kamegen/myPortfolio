1a285ec8e64a4fc51f5fa26595c6b8dc18894ab4
Kamegen <kamenko.gennady@gmail.com>
2023-01-30 20:04:31 +0300

    change Resume and add React

--- script/script.js
+++ script/script.js
@@ -1,20 +1,19 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('mobile-load-animation')
+      web.classList.add('web-load-animation')
+      htmlcss.classList.add('htmlcss-load-animation')
+      js.classList.add('js-load-animation')
+      angular.classList.add('angular-load-animation')
+      react.classList.add('react-load-animation')
+    }
+  })
+})
 
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-        entry.target.classList.add('mobile-load-animation');
-        web.classList.add('web-load-animation');
-        htmlcss.classList.add('htmlcss-load-animation');
-        js.classList.add('js-load-animation');
-        angular.classList.add('angular-load-animation');
-
-        }
-    });
-    });
-
-    observer.observe(document.querySelector('.mobile-load'));
-    const web = document.querySelector('.web-load');
-    const htmlcss = document.querySelector('.htmlcss-load');
-    const js = document.querySelector('.js-load');
-    const angular = document.querySelector('.angular-load');
+observer.observe(document.querySelector('.mobile-load'))
+const web = document.querySelector('.web-load')
+const htmlcss = document.querySelector('.htmlcss-load')
+const js = document.querySelector('.js-load')
+const angular = document.querySelector('.angular-load')
+const react = document.querySelector('.react-load')
